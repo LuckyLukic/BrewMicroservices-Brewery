bf218a789f527d9480b6a09a2094bff062151148
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-11-09 13:26:59 +0100

    pom update and mapper impl

--- src/main/java/luca/springframework/brewery/web/mappers/CustomerMapper.java
+++ src/main/java/luca/springframework/brewery/web/mappers/CustomerMapper.java
@@ -2,7 +2,9 @@ package luca.springframework.brewery.web.mappers;
 
 import luca.springframework.brewery.domain.Customer;
 import luca.springframework.brewery.web.model.CustomerDto;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface CustomerMapper {
 
     CustomerDto customerToCustomerDto (Customer customer);
